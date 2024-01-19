7e6f6d230cf73bb65a7e6f7b8c35b1f13f71291b
Wei Jiang <jiangwei909@gmail.com>
2024-01-19 15:02:56 +0800

    updated user icon

--- src/app/mychat.js
+++ src/app/mychat.js
@@ -97,13 +97,10 @@ function MyChat() {
       <div ref={divRef} className="overflow-auto h-96">
         {messages.map((m, idx) => (
           <div key={idx} className={`flex flex-col p-2 rounded`}>
-            <div className="flex p-2">
+            <div className="flex p-2 items-center	">
               {m.role === "user" && (
                 <div className="flex">
-                  <link
-                    rel="stylesheet"
-                    href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200"
-                  />
+                  <svg xmlns="http://www.w3.org/2000/svg" height="24" viewBox="0 -960 960 960" width="24" fill="#3B82F6"><path d="M620-520q25 0 42.5-17.5T680-580q0-25-17.5-42.5T620-640q-25 0-42.5 17.5T560-580q0 25 17.5 42.5T620-520Zm-280 0q25 0 42.5-17.5T400-580q0-25-17.5-42.5T340-640q-25 0-42.5 17.5T280-580q0 25 17.5 42.5T340-520Zm140 260q68 0 123.5-38.5T684-400H276q25 63 80.5 101.5T480-260Zm0 180q-83 0-156-31.5T197-197q-54-54-85.5-127T80-480q0-83 31.5-156T197-763q54-54 127-85.5T480-880q83 0 156 31.5T763-763q54 54 85.5 127T880-480q0 83-31.5 156T763-197q-54 54-127 85.5T480-80Zm0-400Zm0 320q134 0 227-93t93-227q0-134-93-227t-227-93q-134 0-227 93t-93 227q0 134 93 227t227 93Z"/></svg>
                   <span className="ml-1">You</span>
                 </div>
               )}
